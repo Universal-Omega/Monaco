fc2353089d0e3eb63dbb7347ff323f53bb98bff0
James Haley <haleyjd@hotmail.com>
2016-02-23 15:29:44 -0600

    Revert "Change cherry-picked from Arcane21/monaco-port e094a78 courtesy of Southparkfan - be agnostic about install directory"
    This reverts commit 5c29893bb2e711f3b24f052482fa299d82057fb4.
    
    Breaks relative paths in CSS style sheets.

--- monaco.php
+++ monaco.php
@@ -13,7 +13,7 @@
 
 if( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
 
-$wgExtensionCredits['skin'][] = array(
+$wgExtensionCredits['skin'][] = array (
 	'path' => __FILE__,
 	'name' => 'Monaco',
 	'author' => array('Inez Korczynski', 'Christian Williams', '[http://mediawiki.org/wiki/User:Dantman Daniel Friesen]', '[http://doomwiki.org/wiki/User:Quasar James Haley]'),
@@ -23,9 +23,9 @@ $wgExtensionCredits['skin'][] = array(
 );
 
 $wgValidSkinNames['monaco'] = 'Monaco';
-$wgAutoloadClasses['SkinMonaco'] = dirname(__FILE__) . '/Monaco.skin.php';
-$wgAutoloadClasses['MonacoSidebar'] = dirname(__FILE__) . '/MonacoSidebar.class.php';
-//$wgExtensionMessagesFiles['Monaco'] = dirname(__FILE__) . '/Monaco.i18n.php';
+$wgAutoloadClasses['SkinMonaco'] = dirname(__FILE__).'/Monaco.skin.php';
+$wgAutoloadClasses['MonacoSidebar'] = dirname(__FILE__).'/MonacoSidebar.class.php';
+//$wgExtensionMessagesFiles['Monaco'] = dirname(__FILE__).'/Monaco.i18n.php';
 $wgMessagesDirs['Monaco'] = __DIR__ . '/i18n';
 
 $wgHooks['MessageCacheReplace'][] = 'MonacoSidebar::invalidateCache';
@@ -33,30 +33,28 @@ $wgHooks['MessageCacheReplace'][] = 'MonacoSidebar::invalidateCache';
 $wgResourceModules['skins.monaco'] = array(
 	'position' => 'top',
 	'styles' => array(
-		'style/css/monobook_modified.css' => array( 'media' => 'screen' ),
-		'style/css/reset_modified.css' => array( 'media' => 'screen' ),
-		'style/css/buttons.css' => array( 'media' => 'screen' ),
-		'style/css/sprite.css' => array( 'media' => 'screen' ),
-		'style/css/root.css' => array( 'media' => 'screen' ),
-		'style/css/header.css' => array( 'media' => 'screen' ),
-		'style/css/article.css' => array( 'media' => 'screen' ),
-		'style/css/widgets.css' => array( 'media' => 'screen' ),
-		'style/css/modal.css' => array( 'media' => 'screen' ),
-		'style/css/footer.css' => array( 'media' => 'screen' ),
-		'style/css/star_rating.css' => array( 'media' => 'screen' ),
-		'style/css/ny.css' => array( 'media' => 'screen' ),
-		'style/css/print.css' => array( 'media' => 'print' ),
+		'skins/monaco/style/css/monobook_modified.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/reset_modified.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/buttons.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/sprite.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/root.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/header.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/article.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/widgets.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/modal.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/footer.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/star_rating.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/ny.css' => array( 'media' => 'screen' ),
+		'skins/monaco/style/css/print.css' => array( 'media' => 'print' ),
 	),
-	'scripts' => 'style/js/monaco.js',
-	'localBasePath' => __DIR__
+	'scripts' => 'skins/monaco/style/js/monaco.js',
 );
 
 /* haleyjd 20140809: Widget Framework 2.0 */
 $wgResourceModules['ext.monacoWidget'] = array(
 	'position' => 'bottom',
-	'scripts' => 'ext/ext.monacoWidget.js',
-	'styles'  => 'ext/ext.monacoWidget.css',
-	'localBasePath' => __DIR__
+	'scripts' => 'skins/monaco/ext/ext.monacoWidget.js',
+	'styles'  => 'skins/monaco/ext/ext.monacoWidget.css',
 );
 
 /* Bad Configs - These are left-over junk used inside Monaco.skin.php that should be slowly removed */
