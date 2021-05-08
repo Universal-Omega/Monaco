eaa79798c19a50a4f5b75f29cf14d3450d98af85
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-08 16:16:49 -0600

    Update ExtendedBodyAttributes.php

--- ExtendedBodyAttributes.php
+++ ExtendedBodyAttributes.php
@@ -8,6 +8,25 @@ $wgHooks['OutputPageBodyAttributes'][] = 'egExtendedOutputPageBodyAttributes';
  * @param array &$bodyAttrs
  */
 function egExtendedOutputPageBodyAttributes( OutputPage $out, Skin $skin, &$bodyAttrs ) {
+	$bodyAttrs['class'] .= ' color2';
+		
+	$action = $skin->getRequest()->getVal( 'action' );
+	if ( in_array( $action, [ 'edit', 'history', 'diff', 'delete', 'protect', 'unprotect', 'submit' ] ) ) {
+		$bodyAttrs['class'] .= ' action_' . $action;
+	} elseif ( empty( $action ) || in_array( $action, [ 'view', 'purge' ] ) ) {
+		$bodyAttrs['class'] .= ' action_view';
+	}
+		
+	if ( $skin->showMasthead() ) {
+		if ( $skin->isMastheadTitleVisible() ) {
+			$bodyAttrs['class'] .= ' masthead-special';
+		} else {
+			$bodyAttrs['class'] .= ' masthead-regular';
+		}
+	}
+		
+	$bodyAttrs['id'] = 'body';
+
 	if ( !$skin->getUser()->isRegistered() ) {
 		$bodyAttrs['class'] .= ' loggedout';
 	}
