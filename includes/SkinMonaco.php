286f23562bec82cf3e1ac6a955af70fe59994ce8
paladox <paladox@users.noreply.github.com>
2023-12-15 16:48:39 +0000

    Fix "PHP Deprecated: Creation of dynamic property SkinMonaco::$mMastheadTitleVisible is deprecated" (#26)

--- includes/SkinMonaco.php
+++ includes/SkinMonaco.php
@@ -26,6 +26,9 @@ class SkinMonaco extends SkinTemplate {
 	 */
  	private $config;
 
+	private $mMastheadUser;
+	private $mMastheadTitleVisible;
+
 	public function __construct( array $options = [] ) {
 		$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'monaco' );
 
