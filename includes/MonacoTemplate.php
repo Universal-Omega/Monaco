57547dd2ee1737a3cf602e693c33b7c4f1c5585c
WikiMANNia <55834825+WikiMANNia@users.noreply.github.com>
2024-04-29 10:57:54 +0200

    Cherry picking from haleyjd
    commit 68094fd (https://github.com/haleyjd/monaco-port/commit/68094fd)
    Make contribute button into optional/configurable paypal and patreon …
    
    commit 2a28088 (https://github.com/haleyjd/monaco-port/commit/2a28088)
    Auto overflow in std widget lists
    
    commit 653d2b4 (https://github.com/haleyjd/monaco-port/commit/653d2b4)
    Handle IPv6 in monacoWidget.UserLink

--- includes/MonacoTemplate.php
+++ includes/MonacoTemplate.php
@@ -503,6 +503,32 @@ $this->printRightSidebar() . '
 					$html .= '</ul>
 				</td>
 			</tr>
+<?php
+		global $wgMonacoEnablePaypal, $wgMonacoPaypalID, $wgMonacoEnablePatreon, $wgMonacoPatreonURL;
+		if ( $wgMonacoEnablePaypal ) {
+?>
+			<tr>
+				<td colspan="2" style="text-align:center;">
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" title="PayPal">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="<?php echo $wgMonacoPaypalID ?>">
+						<input type="image" src="<?php $this->text('stylepath') ?>/monaco/style/images/paypal.png" name="submit" alt="PayPal - The safer, easier way to pay online!" style="border: 0; width:139px; margin:0;">
+						<img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" style="border: 0;">
+					</form>
+				</td>
+			</tr>
+<?php
+		}
+		if ( $wgMonacoEnablePatreon ) {
+?>
+			<tr>
+				<td colspan="2" style="text-align:center;">
+					<a href="<?php echo $wgMonacoPatreonURL ?>" target="_blank" rel="nofollow"><img alt="Patreon" src="<?php $this->text('stylepath') ?>/monaco/style/images/patreon.png" width="139" height="37"></a>
+				</td>
+			</tr>
+<?php
+		}
+?>
 		</tbody>';
 	}
 	// END: create static box
