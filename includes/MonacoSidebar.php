9c57ba5d67aa60a5f8c58cc663b346e261f0c27e
WikiMANNia <55834825+WikiMANNia@users.noreply.github.com>
2025-01-07 16:20:08 +0100

    Version 2.8.2

--- includes/MonacoSidebar.php
+++ includes/MonacoSidebar.php
@@ -69,7 +69,7 @@ class MonacoSidebar {
 			if ( !wfMessage( $line_temp[0] )->exists() ) {
 				$link = $line_temp[0];
 			}
-			if ( preg_match( '/^(?:' . wfUrlProtocols() . ')/', $link ) ) {
+			if ( preg_match( '/^(?:' . wfUrlProtocolsWithoutProtRel() . ')/', $link ) ) {
 				$href = $link;
 			} else {
 				$title = Title::newFromText( $link );
@@ -480,7 +480,7 @@ class MonacoSidebar {
 			$link = $lineTmp[0];
 		}
 
-		if ( preg_match( '/^(?:' . wfUrlProtocols() . ')/', $link ) ) {
+		if ( preg_match( '/^(?:' . wfUrlProtocolsWithoutProtRel() . ')/', $link ) ) {
 			$href = $link;
 		} else {
 			if ( empty( $link ) ) {
